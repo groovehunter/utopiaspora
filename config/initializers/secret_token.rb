7f6a9edd59ee8318b1aa07bc72f892c933c3a685
Florian Konnertz <florikon@googlemail.com>
2012-03-02 23:06:54 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Utopiaspora::Application.config.secret_token = 'a4f1add7d7eba86fd3865f1cd3f76c13ee6d7a6deb29a12eeff756e57c9c5f639c9a98f53d2e5d492ce61b2321883619bb73d2ac21bc0410bcea4345cb46228c'
